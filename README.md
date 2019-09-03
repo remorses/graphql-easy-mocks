ad44e67367d50f763fa2c18f3dd809b9ca05aea1
remorses <beats.by.morse@gmail.com>
2019-09-03 20:13:43 +0200

    works

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# nodejs-lib-template
+
+## Usage
+`graphql-easy-mocks --port 9000 -f schema.graphql -m mocks.js`
+
+```javascript
+// mocks.js
+
+const faker = require('faker')
+
+module.exports = {
+    String: () => 'works!',
+    Int: () => faker.random.number(100),
+}
+```
+Then start the server with the app
+```json
+"scripts": {
+    "start": "REACT_APP_TESTING=1 concurrently 'npm:mock-server' 'react-scripts start'",
+    "mock-server": "graphql-easy-mocks -p 9000 -f schema.graphql -m mocks.js",
+}
+```
+Then use it as your server url inside your app.
+
+```js
+import ApolloClient from 'apollo-boost';
+
+const uri = process.env.REACT_APP_TESTING ? 'http://localhost:9000' : 'https://countries.trevorblades.com'
+export default new ApolloClient({
+  uri,
+});
+```
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { ApolloServer } from 'apollo-server'
+import * as fs from 'fs'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
+import faker from 'faker'
+import yargs from 'yargs'
+
+export const main = () => {
+    const argv = yargs
+        .option('port', {
+            alias: 'p',
+            description: '',
+            type: 'number',
+            required: true
+        })
+        .option('schema', {
+            alias: 'f',
+            description: '',
+            type: 'string',
+            default: 'schema.graphql'
+        })
+        .option('mocks', {
+            alias: 'm',
+            description: '',
+            type: 'string',
+            required: true
+        })
+        .help()
+        .alias('help', 'h').argv
+
+    const schemaString = fs.readFileSync(
+        process.cwd() + '/' + argv.schema,
+        'utf8'
+    )
+    const schema = makeExecutableSchema({ typeDefs: schemaString })
+    
+    const userMocks = require(process.cwd() + '/' + argv.mocks)
+
+    const mocks = {
+        Int: () => Math.round(faker.random.number(100)),
+        Float: () => faker.random.number(100) + 0.001,
+        String: () => faker.random.words(2),
+        Bool: faker.random.boolean,
+        ...userMocks
+    }
+
+    addMockFunctionsToSchema({ schema, mocks, preserveResolvers: false })
+
+    const server = new ApolloServer({ schema })
+
+    server.listen(argv.port).then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`)
+    })
+}
