3aa5a0ce23379423fd7777e9f236a501b8ca7674
remorses <beats.by.morse@gmail.com>
2019-10-04 18:37:36 +0200

    made watcher

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer, ServerInfo } from 'apollo-server'
+import chokidar from 'chokidar'
 import * as fs from 'fs'
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
 import faker from 'faker'
 import yargs from 'yargs'
 
-export const main = () => {
+export default async () => {
     const argv = yargs
         .option('port', {
             alias: 'p',
@@ -27,14 +28,38 @@ export const main = () => {
         .help()
         .alias('help', 'h').argv
 
-    const schema = getSchema(process.cwd() + '/' + argv.schema)
-    const userMocks = argv.mocks
-        ? require(process.cwd() + '/' + argv.mocks)
-        : {}
+    const schemaPath = process.cwd() + '/' + argv.schema
+    const mocksPath = argv.mocks ? process.cwd() + '/' + argv.mocks : null
+    const watcher = chokidar.watch(schemaPath)
+    if (mocksPath) watcher.add(mocksPath)
+    let { server } = await main({ mocksPath, schemaPath, port: argv.port })
+    watcher.on('ready', () => {
+        watcher.on('all', async () => {
+            server.close(async () => {
+                console.log('restarting server')
+                const data = await main({
+                    mocksPath,
+                    schemaPath,
+                    port: argv.port
+                })
+                server = data.server
+            })
+        })
+    })
+}
+
+export const main = async ({
+    port,
+    schemaPath,
+    mocksPath
+}): Promise<ServerInfo> => {
+    const schema = getSchema(schemaPath)
+    const userMocks = mocksPath ? require(mocksPath) : {}
     const mocks = {
         Int: () => Math.round(faker.random.number(100)),
         Float: () => faker.random.number(100) + 0.001,
         String: () => faker.random.words(2),
+        Json: () => ({ jsonData: {} }),
         Bool: faker.random.boolean,
         ...userMocks
     }
@@ -42,9 +67,9 @@ export const main = () => {
     addMockFunctionsToSchema({ schema, mocks, preserveResolvers: false })
 
     const server = new ApolloServer({ schema })
-
-    server.listen(argv.port).then(({ url }) => {
-        console.log(`🚀  Server ready at ${url}`)
+    return await server.listen(port).then((data) => {
+        console.log(`🚀  Server ready at ${data.url}`)
+        return data
     })
 }
 
